9a7850159aed05e504d7b3d318b3abb6ff80d432
Danny Thompson <dannymthompson94@gmail.com>
2018-08-22 16:10:59 -0700

    Cleanup.

--- MergeSortExample.java
+++ MergeSortExample.java
@@ -12,16 +12,6 @@ public class MergeSortExample {
 		numbers = toSort;
     }
 
-    // Add the item to the array.
-    public void addToArray(int[] array, int toAdd) {
-    	for(int i = 0; i < array.length; i++) {
-    		if(array[i] == 0) {
-    			array[i] = toAdd;
-    			break;
-    		}
-    	}
-    }
-
     //Recursive method to keep splitting the array into left and right sides, then merge it in order.
     public int[] mergeSort(int[] nums) {
     	int length = nums.length;
@@ -33,15 +23,10 @@ public class MergeSortExample {
     	ArrayList<Integer> leftList = new ArrayList<Integer>();
     	ArrayList<Integer> rightList = new ArrayList<Integer>();
 
-    	// int[] left = new int[length / 2];
-    	// int[] right = new int[length / 2];
-
     	for(int i = 0; i < length; i++) {
     		if(i < length / 2) {
-    			//addToArray(left, nums[i]);
     			leftList.add(nums[i]);
     		} else {
-    			//addToArray(right, nums[i]);
     			rightList.add(nums[i]);
     		}
     	}
@@ -54,20 +39,6 @@ public class MergeSortExample {
     	right = mergeSort(right);
 
     	return merge(left, right);
-    }
-
-    /**
-     * Returns the array, minus the first element.
-     */
-    public int[] arraySansFirst(int[] toRemove) {
-    	int length = toRemove.length;
-    	int[] toReturn = new int[length - 1];
-
-    	for(int i = 1; i < length; i++ ) {
-    		toReturn[i - 1] = toRemove[i];
-    	}
-
-    	return toReturn;
     }
 
     //Merge the left and right sub-arrays.
@@ -97,13 +68,40 @@ public class MergeSortExample {
     	return sorted;
     }
 
+    /******************
+     * Helper Methods *
+     ******************/
+
+    // Add the item to the array.
+    public void addToArray(int[] array, int toAdd) {
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] == 0) {
+                array[i] = toAdd;
+                break;
+            }
+        }
+    }
+
+    /**
+     * Returns the array, minus the first element.
+     */
+    public int[] arraySansFirst(int[] toRemove) {
+        int length = toRemove.length;
+        int[] toReturn = new int[length - 1];
+
+        for(int i = 1; i < length; i++ ) {
+            toReturn[i - 1] = toRemove[i];
+        }
+
+        return toReturn;
+    }
+
     //Used to print the given array.
     public void print(int[] toPrint) {
 		for(int i = 0; i < toPrint.length; i++) {
 	    	System.out.print(toPrint[i] + " ");
 		}
     }
-
 
     public static void main(String[] args) {
 	int[] toSort = {1, 10, 8, 3, 9, 50, 24, 2, 55, 60, 9};
